dab28c768da030af81d4a9306ad5f6566b191eb2
Michael Donaghy <md401@srcf.ucam.org>
2015-10-03 23:23:39 +0100

    maybe it looks like that?
    Signed-off-by: Michael Donaghy <md401@srcf.ucam.org>

--- src/main/scala/com/github/m50d/paperdoll/Queue.scala
+++ src/main/scala/com/github/m50d/paperdoll/Queue.scala
@@ -1,10 +1,14 @@
 package com.github.m50d.paperdoll
 
-trait P[C[_, _], A, B] {
-  def cs[W](f: C[A, W], s: C[W, B]): P[C, A, B]
-}
+sealed trait P[C[_, _], A, B]
+case class CS[C[_, _], A, B, W](v: C[A, W] => C[W, B]) extends P[C, A, B]
 
-//sealed trait B[C[_, _], A, B]
-//trait B1[C[_, _], A, B0] extends B[C, A, B0] {
-//  def 
-//}
+sealed trait B[C[_, _], A, B]
+case class B1[C[_, _], A, B0](v: C[A, B0]) extends B[C, A, B0]
+case class B2[C[_, _], A, B0](v: P[C, A, B0]) extends B[C, A, B0]
+
+sealed trait Queue[C[_, _], A, B]
+case class Q0[C[_, _], A]() extends Queue[C, A, A]
+case class Q1[C[_, _], A, B](v: C[A, B]) extends Queue[C, A, B]
+case class QN[C[_, _], A, B0, X, Y](
+    v: (B[C, A, X], Queue[({type L[A, B] = P[C, A, B]})#L, X, Y] => B[C, Y, B])
