5e571e172877de517e82921e5e336db80308e49c
Michael Donaghy <md401@srcf.ucam.org>
2015-10-03 23:34:47 +0100

    urgh is this it?
    Signed-off-by: Michael Donaghy <md401@srcf.ucam.org>

--- src/main/scala/com/github/m50d/paperdoll/Queue.scala
+++ src/main/scala/com/github/m50d/paperdoll/Queue.scala
@@ -1,7 +1,11 @@
 package com.github.m50d.paperdoll
 
+trait Forall1[C[_, _]] {
+  def apply[X, Y]: C[X, Y]
+}
+
 sealed trait P[C[_, _], A, B]
-case class CS[C[_, _], A, B, W](v: C[A, W] => C[W, B]) extends P[C, A, B]
+case class CS[C[_, _], A, B, W](v1: C[A, W], v2 C[W, B]) extends P[C, A, B]
 
 sealed trait B[C[_, _], A, B]
 case class B1[C[_, _], A, B0](v: C[A, B0]) extends B[C, A, B0]
@@ -11,4 +15,11 @@ sealed trait Queue[C[_, _], A, B]
 case class Q0[C[_, _], A]() extends Queue[C, A, A]
 case class Q1[C[_, _], A, B](v: C[A, B]) extends Queue[C, A, B]
 case class QN[C[_, _], A, B0, X, Y](
-    v: (B[C, A, X], Queue[({type L[A, B] = P[C, A, B]})#L, X, Y] => B[C, Y, B])
+  v: (B[C, A, X], Queue[({ type L[A, B] = P[C, A, B] })#L, X, Y]) ⇒ B[C, Y, B0]) extends Queue[C, A, B0]
+
+object Queue {
+  def tmapp[C[_, _], D[_, _]](f: Forall1[({type L[X, Y]} = (C[X, Y]) => D[X, Y])#L])(phi: P[C, X, Y]): P[D, X, Y] =
+    phi match {
+    case CS(v1, v2) => CS(f(v1), f(v2)) 
+  }
+}
