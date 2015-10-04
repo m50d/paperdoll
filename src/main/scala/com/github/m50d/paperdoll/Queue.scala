e29bb45f29945f8d540944ad83890c768c0481ee
Michael Donaghy <md401@srcf.ucam.org>
2015-10-04 11:41:43 +0100

    making a little progress
    Signed-off-by: Michael Donaghy <md401@srcf.ucam.org>

--- src/main/scala/com/github/m50d/paperdoll/Queue.scala
+++ src/main/scala/com/github/m50d/paperdoll/Queue.scala
@@ -7,29 +7,49 @@ trait FunctionKK[C[_, _], D[_, _]] {
   def apply[X, Y](c: C[X, Y]): D[X, Y]
 }
 
+sealed trait TAViewL[S[_[_, _], _, _], C[_, _], X, Y]
+
+final case class TAEmptyL[S[_[_, _], _, _], C[_, _], X]() extends TAViewL[S, C, X, X]
+final case class :<[S[_[_, _], _, _], C[_, _], X, Y, Z](e: C[X, Y], s: S[C, Y, Z]) extends TAViewL[S, C, X, Z]
+
 sealed trait P[C[_, _], A, B]
-final case class CS[C[_, _], A, B, W](v1: C[A, W], v2: C[W, B]) extends P[C, A, B]
+final case class CS[C[_, _], A, B, W](a: C[A, W], b: C[W, B]) extends P[C, A, B]
 
 sealed trait B[C[_, _], A, B]
-final case class B1[C[_, _], A, B0](v: C[A, B0]) extends B[C, A, B0]
+final case class B1[C[_, _], A, B0](a: C[A, B0]) extends B[C, A, B0]
 final case class B2[C[_, _], A, B0](v: P[C, A, B0]) extends B[C, A, B0]
 
 sealed trait Queue[C[_, _], A, B] {
   def |>[Z](e: C[B, Z]): Queue[C, A, Z]
+  def tviewl: TAViewL[Queue, C, A, B]
 }
 final case class Q0[C[_, _], A]() extends Queue[C, A, A] {
   override def |>[Z](e: C[A, Z]) = Q1(e) 
+  override def tviewl = TAEmptyL()
 }
-final case class Q1[C[_, _], A, B](v: C[A, B]) extends Queue[C, A, B] {
+final case class Q1[C[_, _], A, B](a: C[A, B]) extends Queue[C, A, B] {
   override def  |>[Z](e: C[B, Z]) =
-    QN[C, A, Z, B, B](B1(v), Q0[({ type L[A, B] = P[C, A, B] })#L, B](), B1(e))
+    QN[C, A, Z, B, B](B1(a), Q0[({ type L[A, B] = P[C, A, B] })#L, B](), B1(e))
+  override def tviewl = :<(a, Q0())
 }
 final case class QN[C[_, _], A, B0, X, Y](
-  v1: B[C, A, X], v2: Queue[({ type L[A, B] = P[C, A, B] })#L, X, Y], v3:  B[C, Y, B0]) extends Queue[C, A, B0] {
+  l: B[C, A, X], m: Queue[({ type L[A, B] = P[C, A, B] })#L, X, Y], r:  B[C, Y, B0]) extends Queue[C, A, B0] {
   override def |>[Z](e: C[B0, Z]) =
-    v3 match {
-    case B1(a) => QN(v1, v2, B2(CS(a, e)))
-    case B2(r) => QN(v1, v2 |> r, B1(e))
+    r match {
+    case B1(a) => QN(l, m, B2(CS(a, e)))
+    case B2(r) => QN(l, m |> r, B1(e))
+  }
+  override def tviewl = l match {
+    case B2(CS(a, b)) => :<(a, QN(B1(b), m, r))
+    case B1(a) => {
+      def buf2queue[Z, W](b: B[C, Z, W]) = b match {
+        case B1(a) => Q1(a)
+//        case B2(CS(a, b)) => QN(B1(a), Q0[({ type L[A, B] = P[C, A, B] })#L, ](), B1(b))
+      } 
+      
+//      def shiftLeft
+      ???
+    }
   }
 }
 
