2215f9d18644e81fca5bf6e524e0f99ce1beeca1
Michael Donaghy <md401@srcf.ucam.org>
2015-10-04 17:19:46 +0100

    Switch to scalaz for Leibniz, make progress
    Signed-off-by: Michael Donaghy <md401@srcf.ucam.org>

--- src/main/scala/com/github/m50d/paperdoll/Queue.scala
+++ src/main/scala/com/github/m50d/paperdoll/Queue.scala
@@ -1,5 +1,7 @@
 package com.github.m50d.paperdoll
 
+import scalaz.Leibniz.===
+
 /**
  * Function1 for types of kind [_, _]
  */
@@ -9,11 +11,23 @@ trait FunctionKK[C[_, _], D[_, _]] {
 
 sealed trait TAViewL[S[_[_, _], _, _], C[_, _], X, Y]
 
-final case class TAEmptyL[S[_[_, _], _, _], C[_, _], X]() extends TAViewL[S, C, X, X]
-final case class :<[S[_[_, _], _, _], C[_, _], X, Y, Z](e: C[X, Y], s: S[C, Y, Z]) extends TAViewL[S, C, X, Z]
+final case class TAEmptyL[S[_[_, _], _, _], C[_, _], X, Y]()(implicit val witness: Y === X) extends TAViewL[S, C, X, Y]
+final case class :<[S[_[_, _], _, _], C[_, _], X, Y0, Z](e: C[X, Y0], s: S[C, Y0, Z]) extends TAViewL[S, C, X, Z] {
+  type Y = Y0
+}
 
 sealed trait P[C[_, _], A, B]
-final case class CS[C[_, _], A, B, W](a: C[A, W], b: C[W, B]) extends P[C, A, B]
+
+/**
+ * Partially applied P to make it easier to write types
+ */
+sealed trait P_[C[_, _]] {
+ final type O[X, Y] = P[C, X, Y] 
+}
+
+final case class CS[C[_, _], A, B, W0](a: C[A, W0], b: C[W0, B]) extends P[C, A, B] {
+  type W = W0
+}
 
 sealed trait B[C[_, _], A, B]
 final case class B1[C[_, _], A, B0](a: C[A, B0]) extends B[C, A, B0]
@@ -29,11 +43,11 @@ final case class Q0[C[_, _], A]() extends Queue[C, A, A] {
 }
 final case class Q1[C[_, _], A, B](a: C[A, B]) extends Queue[C, A, B] {
   override def  |>[Z](e: C[B, Z]) =
-    QN[C, A, Z, B, B](B1(a), Q0[({ type L[A, B] = P[C, A, B] })#L, B](), B1(e))
+    QN[C, A, Z, B, B](B1(a), Q0[P_[C]#O, B](), B1(e))
   override def tviewl = :<(a, Q0())
 }
 final case class QN[C[_, _], A, B0, X, Y](
-  l: B[C, A, X], m: Queue[({ type L[A, B] = P[C, A, B] })#L, X, Y], r:  B[C, Y, B0]) extends Queue[C, A, B0] {
+  l: B[C, A, X], m: Queue[P_[C]#O, X, Y], r:  B[C, Y, B0]) extends Queue[C, A, B0] {
   override def |>[Z](e: C[B0, Z]) =
     r match {
     case B1(a) => QN(l, m, B2(CS(a, e)))
@@ -42,12 +56,15 @@ final case class QN[C[_, _], A, B0, X, Y](
   override def tviewl = l match {
     case B2(CS(a, b)) => :<(a, QN(B1(b), m, r))
     case B1(a) => {
-      def buf2queue[Z, W](b: B[C, Z, W]) = b match {
+      def buf2queue[Z, W](b: B[C, Z, W]): Queue[C, Z, W] = b match {
         case B1(a) => Q1(a)
-//        case B2(CS(a, b)) => QN(B1(a), Q0[({ type L[A, B] = P[C, A, B] })#L, ](), B1(b))
+        case B2(cs @ CS(a, b)) => QN(B1(a), Q0[P_[C]#O, cs.W](), B1(b))
       } 
-      
-//      def shiftLeft
+//      def shiftLeft[A, B3, W](q: Queue[({ type L[A, B] = P[C, A, B] })#L, A, W], r: B[C, W, B3]): Queue[C, A, B3] =
+//        q.tviewl match {
+//        case tael @ TAEmptyL() => buf2queue(tael.witness.subst[({type L[V] = B[C, V, B3]})#L](r))
+//        case cl @ :<(l, m) => QN(B2(l), m, r)
+//      }
       ???
     }
   }
