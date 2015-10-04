342b8ba8baf345aef3df0e48ee4c9cb5702e9bf4
Michael Donaghy <md401@srcf.ucam.org>
2015-10-04 11:04:42 +0100

    Realise I've been not compiling all this time
    Signed-off-by: Michael Donaghy <md401@srcf.ucam.org>

--- src/main/scala/com/github/m50d/paperdoll/Queue.scala
+++ src/main/scala/com/github/m50d/paperdoll/Queue.scala
@@ -5,21 +5,28 @@ trait Forall1[C[_, _]] {
 }
 
 sealed trait P[C[_, _], A, B]
-case class CS[C[_, _], A, B, W](v1: C[A, W], v2 C[W, B]) extends P[C, A, B]
+case class CS[C[_, _], A, B, W](v1: C[A, W], v2: C[W, B]) extends P[C, A, B]
 
 sealed trait B[C[_, _], A, B]
 case class B1[C[_, _], A, B0](v: C[A, B0]) extends B[C, A, B0]
 case class B2[C[_, _], A, B0](v: P[C, A, B0]) extends B[C, A, B0]
 
-sealed trait Queue[C[_, _], A, B]
+sealed trait Queue[C[_, _], A, B] {
+  def |>[Z](e: C[B, Z]): Queue[C, A, Z]
+}
 case class Q0[C[_, _], A]() extends Queue[C, A, A]
 case class Q1[C[_, _], A, B](v: C[A, B]) extends Queue[C, A, B]
 case class QN[C[_, _], A, B0, X, Y](
   v: (B[C, A, X], Queue[({ type L[A, B] = P[C, A, B] })#L, X, Y]) ⇒ B[C, Y, B0]) extends Queue[C, A, B0]
 
 object Queue {
-  def tmapp[C[_, _], D[_, _]](f: Forall1[({type L[X, Y]} = (C[X, Y]) => D[X, Y])#L])(phi: P[C, X, Y]): P[D, X, Y] =
+  def tmapp[C[_, _], D[_, _]](f: Forall1[({type L[X, Y] = (C[X, Y]) => D[X, Y]})#L])(phi: P[C, X, Y]): P[D, X, Y] =
     phi match {
-    case CS(v1, v2) => CS(f(v1), f(v2)) 
+    case CS(v1, v2) => CS(f.apply(v1), f.apply(v2)) 
+  }
+  def tmapb[C[_, _], D[_, _]](f: Forall1[({type L[X, Y] = (C[X, Y]) => D[X, Y]})#L])(phi: B[C, X, Y]): B[C, X, Y] =
+    phi match {
+    case B1(v) => B1(f.apply(v))
+    case B2(v) => B2(tmapp(f)(v))
   }
 }
