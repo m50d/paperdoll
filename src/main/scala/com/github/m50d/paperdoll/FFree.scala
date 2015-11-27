cfd95773d88eca5a469128efea83ac0e74e0de1a
Michael Donaghy <md401@srcf.ucam.org>
2015-11-27 22:01:44 +0000

    Monad instance
    Signed-off-by: Michael Donaghy <md401@srcf.ucam.org>

--- src/main/scala/com/github/m50d/paperdoll/FFree.scala
+++ src/main/scala/com/github/m50d/paperdoll/FFree.scala
@@ -1,6 +1,7 @@
 package com.github.m50d.paperdoll
 
 import shapeless.Coproduct
+import scalaz.Monad
 
 object aliases {
   type Arr[R <: Coproduct, A, B] = A => FFree[R, B]
@@ -9,14 +10,26 @@ object aliases {
   }
   type Arrs[R <: Coproduct, A, B] = Queue[Arr_[R]#O, A, B]
 }
+import aliases._
 
 sealed trait FFree[R <: Coproduct, A]
 final case class Pure[R <: Coproduct, A](a: A) extends FFree[R, A]
 final case class Impure[R <: Coproduct, X, A](
     eff: X, //No, X is still inside stockings at this point
-    step: X => FFree[R, A]
+    step: Arrs[R, X, A]
 ) extends FFree[R, A]
 
 sealed trait FFree_[R <: Coproduct] {
   final type O[A] = FFree[R, A]
+}
+
+object FFree {
+  implicit def monadFFree[R <: Coproduct] = new Monad[FFree_[R]#O] {
+    override def point[A](a: => A) = Pure(a)
+    override def bind[A, B](fa: FFree[R, A])(f: A => FFree[R, B]) =
+      fa match {
+      case Pure(x) => f(x)
+      case Impure(u, q) => Impure(u, q |> f)
+    }
+  }
 }
