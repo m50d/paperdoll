44cdc72316a96bb215e2c718b70ec4279d6dd558
Michael Donaghy <md401@srcf.ucam.org>
2015-12-24 21:30:19 +0000

    Unapply instance for layers, awesome

--- src/test/scala/com/github/m50d/paperdoll/ReaderTest.scala
+++ src/test/scala/com/github/m50d/paperdoll/ReaderTest.scala
@@ -1,11 +1,21 @@
 package com.github.m50d.paperdoll
 
 import org.junit.Test
-import scalaz.syntax.functor._
 import scalaz.syntax.monad._
-import scalaz.syntax.bind._
+import shapeless.{CNil, :+:, Coproduct}
+import example.Reader_
+import scalaz.Unapply
+import scalaz.Leibniz
 
 class ReaderTest {
+  implicit def unapplyEff[TC[_[_]], R <: Coproduct, A0](
+		  implicit instance: TC[Eff_[R]#O]
+  ) = new Unapply[TC, Eff[R, A0]]{
+    override type A = A0
+    override type M[X] = Eff[R, X]
+    override val TC = instance
+    override val leibniz = Leibniz.refl[Eff[R, A0]]
+  }
 
   @Test def basicFunctionality(): Unit = {
     val reader =
