cce892582ba50a0ff91cdfce80ad63ca4ec2bd4c
Michael Donaghy <md401@srcf.ucam.org>
2016-02-20 13:42:48 +0000

    Remove warnings

--- src/test/scala/com/github/m50d/paperdoll/QueueTest.scala
+++ src/test/scala/com/github/m50d/paperdoll/QueueTest.scala
@@ -8,10 +8,10 @@ class QueueTest {
   @Test def basicFunctionality(): Unit = {
     val queue: Queue[Function1, Int, Int] = Q0[Function1, Int]() |> (_.toString) |> (_.length)
     queue.tviewl match {
-      case ca: :<[Queue, Function1, Int, String, Int] =>
+      case (ca: :<[Queue, Function1, Int, String, Int] @unchecked) =>
         assertThat(ca.e(4)).isEqualTo("4")
         ca.s.tviewl match {
-          case ca: :<[Queue, Function1, String, Int, Int] =>
+          case (ca: :<[Queue, Function1, String, Int, Int] @unchecked) =>
             assertThat(ca.e("hello")).isEqualTo(5)
             ca.s.tviewl match {
               case TAEmptyL() =>
