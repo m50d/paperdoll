38b5e4e3a458ce12d9af14140ec1e1b5756a5aaf
Michael Donaghy <md401@srcf.ucam.org>
2016-02-28 10:18:54 +0000

    Might be what we need

--- src/main/scala/com/github/m50d/paperdoll/layer/Member.scala
+++ src/main/scala/com/github/m50d/paperdoll/layer/Member.scala
@@ -2,16 +2,18 @@ package com.github.m50d.paperdoll.layer
 
 import shapeless.{ Coproduct, :+:, Inl, Inr, CNil }
 
-sealed trait Member[R <: Coproduct, L <: Layers[R], R1 <: Layer] {
+sealed trait Member[R <: Coproduct, R1 <: Layer] {
+  type L <: Layers[R]
   type RestR <: Coproduct
   type RestL <: Layers[RestR]
   def remove[X](value: L#O[X]): Either[R1#F[X], RestL#O[X]]
 }
 
 object Member {
-  implicit def nil[R1 <: Layer, R <: Coproduct](implicit rest: Layers[R]) = new Member[R1 :+: R, Layers[R1 :+: R] {
-    type O[X] = R1#F[X] :+: rest.O[X]
-  }, R1] {
+  implicit def nil[R1 <: Layer, R <: Coproduct](implicit rest: Layers[R]) = new Member[R1 :+: R, R1] {
+    override type L = Layers[R1 :+: R] {
+      type O[X] = R1#F[X] :+: rest.O[X]
+    }
     override type RestR = R
     override type RestL = Layers.Aux[R, rest.O] // i.e. rest.type
     def remove[X](value: R1#F[X] :+: rest.O[X]) = value match {
@@ -20,20 +22,21 @@ object Member {
     }
   }
 
-  implicit def cons[R2 <: Layer, R <: Coproduct, L <: Layers[R], R1 <: Layer](
-    implicit rest: Member[R, L, R1]) =
-    new Member[R2 :+: R, Layers[R2 :+: R] {
-      type O[X] = R2#F[X] :+: L#O[X]
-    }, R1] {
-	  override type RestR = R2 :+: rest.RestR
-	  override type RestL = Layers[RestR] {
-	    type O[X] = R2#F[X] :+: rest.RestL#O[X]
-	  }
-	  override def remove[X](value: R2#F[X] :+: L#O[X]) = value match {
-	    case Inl(x) => Right(Inl(x))
-	    case Inr(r) => rest.remove(r).right.map(Inr(_))
-	  }
+  implicit def cons[R2 <: Layer, R <: Coproduct, R1 <: Layer](
+    implicit rest: Member[R, R1]) =
+    new Member[R2 :+: R, R1] {
+      override type L = Layers[R2 :+: R] {
+        type O[X] = R2#F[X] :+: rest.L#O[X]
+      }
+      override type RestR = R2 :+: rest.RestR
+      override type RestL = Layers[RestR] {
+        type O[X] = R2#F[X] :+: rest.RestL#O[X]
+      }
+      override def remove[X](value: R2#F[X] :+: rest.L#O[X]) = value match {
+        case Inl(x) => Right(Inl(x))
+        case Inr(r) => rest.remove(r).right.map(Inr(_))
+      }
     }
-  
-  def apply[R <: Coproduct, L <: Layers[R], R1 <: Layer](implicit m: Member[R, L, R1]) = m
+
+  def apply[R <: Coproduct, R1 <: Layer](implicit m: Member[R, R1]) = m
 }
--- src/test/scala/com/github/m50d/paperdoll/layer/MemberTest.scala
+++ src/test/scala/com/github/m50d/paperdoll/layer/MemberTest.scala
@@ -1,22 +1,14 @@
 package com.github.m50d.paperdoll.layer
 
 import org.junit.Test
-import shapeless.{CNil, :+:}
+import shapeless.{ CNil, :+: }
 import com.github.m50d.paperdoll.reader.Reader_
 
 class MemberTest {
 
   @Test def basicFunctionality(): Unit = {
-    val l1 = Layers[Reader_[Int] :+: CNil]
-    Member[Reader_[Int] :+: CNil, Layers.Aux[Reader_[Int] :+: CNil, l1.O], Reader_[Int]]
-    
-    val l2 = Layers[Reader_[String] :+: Reader_[Int] :+: CNil]
-    Member[Reader_[String] :+: Reader_[Int] :+: CNil, Layers.Aux[Reader_[String] :+: Reader_[Int] :+: CNil, l2.O], Reader_[String]]
-    //FIXME why doesn't it infer these types?
-    Member[Reader_[String] :+: Reader_[Int] :+: CNil, Layers.Aux[Reader_[String] :+: Reader_[Int] :+: CNil, l2.O], Reader_[Int]](
-    	Member.cons[Reader_[String], Reader_[Int] :+: CNil, Layers.Aux[Reader_[Int] :+: CNil, l1.O], Reader_[Int]]
-    )
-    
-    
+    Member[Reader_[Int] :+: CNil, Reader_[Int]]
+    Member[Reader_[String] :+: Reader_[Int] :+: CNil, Reader_[String]]
+    Member[Reader_[String] :+: Reader_[Int] :+: CNil, Reader_[Int]]
   }
 }
