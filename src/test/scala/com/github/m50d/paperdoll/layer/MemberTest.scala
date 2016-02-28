6aaad991fbc4bc4929d75f13ed23bd087ce4158c
Michael Donaghy <md401@srcf.ucam.org>
2016-02-28 10:09:18 +0000

    Demonstrating the problem

--- src/test/scala/com/github/m50d/paperdoll/layer/MemberTest.scala
+++ src/test/scala/com/github/m50d/paperdoll/layer/MemberTest.scala
@@ -7,7 +7,16 @@ import com.github.m50d.paperdoll.reader.Reader_
 class MemberTest {
 
   @Test def basicFunctionality(): Unit = {
-    val l = Layers[Reader_[Int] :+: CNil]
-    Member[Reader_[Int] :+: CNil, Layers.Aux[Reader_[Int] :+: CNil, l.O], Reader_[Int]]
+    val l1 = Layers[Reader_[Int] :+: CNil]
+    Member[Reader_[Int] :+: CNil, Layers.Aux[Reader_[Int] :+: CNil, l1.O], Reader_[Int]]
+    
+    val l2 = Layers[Reader_[String] :+: Reader_[Int] :+: CNil]
+    Member[Reader_[String] :+: Reader_[Int] :+: CNil, Layers.Aux[Reader_[String] :+: Reader_[Int] :+: CNil, l2.O], Reader_[String]]
+    //FIXME why doesn't it infer these types?
+    Member[Reader_[String] :+: Reader_[Int] :+: CNil, Layers.Aux[Reader_[String] :+: Reader_[Int] :+: CNil, l2.O], Reader_[Int]](
+    	Member.cons[Reader_[String], Reader_[Int] :+: CNil, Layers.Aux[Reader_[Int] :+: CNil, l1.O], Reader_[Int]]
+    )
+    
+    
   }
 }
