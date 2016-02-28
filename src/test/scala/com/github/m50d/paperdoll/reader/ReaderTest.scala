13e24c5ba44a7acf89dfeb9304160d3f092f60ff
Michael Donaghy <md401@srcf.ucam.org>
2016-02-28 16:06:43 +0000

    Potential approach

--- src/test/scala/com/github/m50d/paperdoll/reader/ReaderTest.scala
+++ src/test/scala/com/github/m50d/paperdoll/reader/ReaderTest.scala
@@ -18,7 +18,7 @@ class ReaderTest {
 
     val lr = Layers[Reader_[Int] :+: CNil]
     val l0 = Layers[CNil]
-    val pure = runReader[Int, Reader_[Int] :+: CNil, Layers.Aux[Reader_[Int] :+: CNil, lr.O], CNil, Layers.Aux[CNil, l0.O], Int](4, reader)
+    val pure = runReader[Int, Reader_[Int] :+: CNil, Layers.Aux[Reader_[Int] :+: CNil, lr.O], CNil, Layers.Aux[CNil, l0.O], Layers.Aux[Reader_[Int] :+: CNil, lr.O], Int](4, reader)
     val result = Eff.run(pure)
     assertThat(result).isEqualTo(8)
   }
