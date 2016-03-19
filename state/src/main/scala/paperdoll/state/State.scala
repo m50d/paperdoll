5efdc14c84a0f579389e74385ff31100de0de745
Michael Donaghy <md401@srcf.ucam.org>
2016-03-19 22:38:36 +0000

    Take a guess at how inference is going to work
    Signed-off-by: Michael Donaghy <md401@srcf.ucam.org>

--- state/src/main/scala/paperdoll/state/State.scala
+++ state/src/main/scala/paperdoll/state/State.scala
@@ -17,34 +17,47 @@ import paperdoll.core.effect.Arr
 
 final class StateRunner[S] {
   def loop[R <: Coproduct, L0 <: Layers[R], A, RR <: Coproduct, RL <: Layers[RR]](
-    eff: Eff[R, L0, A], s: S)(implicit m1: Member[R, Writer_[S]] {
+    eff: Eff[R, L0, A], s: S)(implicit m0: Member[R, Writer_[S]] {
       type L = L0
       type RestR = RR
       type RestL = RL
-    }, m2: Member[R, Reader_[S]] {
+    }, m1: Member[R, Reader_[S]] {
       type L = RL
-    }): Eff[m2.RestR, m2.RestL, (A, S)] = eff.fold[Eff[m2.RestR, m2.RestL, (A, S)]](
+    }): Eff[m1.RestR, m1.RestL, (A, S)] = eff.fold[Eff[m1.RestR, m1.RestL, (A, S)]](
     a ⇒ Pure((a, s)),
-    new Forall[({ type K[X] = (L0#O[X], Arrs[R, L0, X, A]) ⇒ Eff[m2.RestR, m2.RestL, (A, S)] })#K] {
+    new Forall[({ type K[X] = (L0#O[X], Arrs[R, L0, X, A]) ⇒ Eff[m1.RestR, m1.RestL, (A, S)] })#K] {
       override def apply[X] = (eff, cont) ⇒ {
         def newCont(s: S) = Eff.compose(cont) andThen { e ⇒ loop(e, s) }
-        m1.remove(eff).fold(
-          effr ⇒ m2.remove(effr).fold(
-            effrr ⇒ Impure[m2.RestR, m2.RestL, X, (A, S)](effrr, Queue.one[Arr_[m2.RestR, m2.RestL]#O, X, (A, S)](
+        m0.remove(eff).fold(
+          effr ⇒ m1.remove(effr).fold(
+            effrr ⇒ Impure[m1.RestR, m1.RestL, X, (A, S)](effrr, Queue.one[Arr_[m1.RestR, m1.RestL]#O, X, (A, S)](
               newCont(s))),
-            _.fold(witness ⇒ Leibniz.symm[Nothing, Any, S, X](witness).subst[({ type K[Y] = Arr[m2.RestR, m2.RestL, Y, (A, S)] })#K](newCont(s))(s))),
-          _.fold((newS, witness) ⇒ witness.subst[({type K[Y] = Arr[m2.RestR, m2.RestL, Y, (A, S)]})#K](newCont(newS))({})))
+            _.fold(witness ⇒ Leibniz.symm[Nothing, Any, S, X](witness).subst[({ type K[Y] = Arr[m1.RestR, m1.RestL, Y, (A, S)] })#K](newCont(s))(s))),
+          _.fold((newS, witness) ⇒ witness.subst[({ type K[Y] = Arr[m1.RestR, m1.RestL, Y, (A, S)] })#K](newCont(newS))({})))
       }
     })
 
-  //  def apply[R <: Coproduct, L0 <: Layers[R], A, L1 <: Layers[R], RR <: Coproduct, RL0 <: Layers[RR], RL1 <: Layers[RR]](
-  //    eff: Eff[R, L0, A], s: S)(implicit m1: Member[R, Writer_[S]] {
-  //      type L = L1
-  //      type RestR = RR
-  //      type RestL = RL0
-  //    }, m2: Member[R, Reader_[S]] {
-  //      type L = RL1
-  //    }, l0: Leibniz[Nothing, Layers[R], L0, L1], l1: Leibniz[Nothing, Layers[RR], RL0, RL1]): Eff[m2.RestR, m2.RestL, (A, S)] = 
+  def apply[R <: Coproduct, L0 <: Layers[R], A, L1 <: Layers[R], RR <: Coproduct, RL0 <: Layers[RR], RL1 <: Layers[RR]](
+    eff: Eff[R, L0, A], s: S)(implicit m0: Member[R, Writer_[S]] {
+      type L = L1
+      type RestR = RR
+      type RestL = RL0
+    }, m1: Member[R, Reader_[S]] {
+      type L = RL1
+    }, l0: Leibniz[Nothing, Layers[R], L1, L0], l1: Leibniz[Nothing, Layers[RR], RL1, RL0]): Eff[m1.RestR, m1.RestL, (A, S)] =
+    loop[R, L0, A, RR, RL0](eff, s)(l0.subst[({
+      type K[LL <: Layers[R]] = Member[R, Writer_[S]] {
+        type L = LL
+        type RestR = RR
+        type RestL = RL0
+      }
+    })#K](m0), l1.subst[({
+      type K[RLL <: Layers[RR]] = Member[R, Reader_[S]] {
+        type L = RLL
+        type RestR = m1.RestR
+        type RestL = m1.RestL
+      }
+    })#K](m1))
 }
 
 object State {
