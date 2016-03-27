aeabe0d3264d3f133a29b69bf8bd34616d0350e7
Michael Donaghy <md401@srcf.ucam.org>
2016-03-27 18:50:32 +0100

    Add SafeForRegion constraint

---
+++ arm/src/main/scala/paperdoll/arm/SafeForRegion.scala
@@ -0,0 +1,16 @@
+package paperdoll.arm
+
+import paperdoll.core.layer.Layer
+import paperdoll.reader.Reader_
+import paperdoll.writer.Writer_
+
+/**
+ * Marker that effects from the given layer can be used safely with regions
+ * (managed resources).
+ */
+trait SafeForRegion[L <: Layer]
+
+object SafeForRegion {
+  implicit def readerSafeForRegion[I] = new SafeForRegion[Reader_[I]]{}
+  implicit def writerSafeForRegion[O] = new SafeForRegion[Writer_[O]]{}
+}
