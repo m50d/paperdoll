22c02e6326e9bbf9bcfadc4a9ae31176e0eae56d
Michael Donaghy <md401@srcf.ucam.org>
2016-03-27 20:32:08 +0100

    Close to implementing the region bit

--- arm/src/main/scala/paperdoll/arm/Region.scala
+++ arm/src/main/scala/paperdoll/arm/Region.scala
@@ -5,12 +5,26 @@ import paperdoll.core.effect.Eff
 import resource.ManagedResource
 import resource.managed
 import scala.reflect.Manifest
+import scalaz.Leibniz
+import paperdoll.core.layer.Member
+import paperdoll.core.layer.Layers
+import shapeless.Coproduct
+import scalaz.Leibniz.===
+import shapeless.UnaryTCConstraint
 
-sealed trait Region[S, R, A]
-
-private[arm] final case class SHandle[S, R](handle: ManagedResource[R]) extends Region[S, R, R]
+sealed trait Region[S, R, A] {
+  def fold[B](resource: (A === R, ManagedResource[R]) => B): B
+}
 
 object Region {
   def newSHandle[R: Resource: Manifest](r: => R): Eff.One[Region_[S, R], R]#O forSome { type S } =
-    Eff.send[Region_[Any, R], R](SHandle(managed(r)))
+    Eff.send[Region_[Any, R], R](new Region[Any, R, R] {
+      override def fold[B](resource: (R === R, ManagedResource[R]) => B) =
+        resource(Leibniz.refl, managed(r))
+    })
+
+//  def newRgn[S, RE, R <: Coproduct, L1 <: Layers[R], A, L2 <: Layers[R]](eff: Eff[R, L1, A])(
+//    implicit me: Member[R, Region_[S, RE]] { type L = L2 },
+//    le: Leibniz[Nothing, Layers[R], L1, L2],
+//    safe: UnaryTCConstraint[R, SafeForRegion]): Eff[me.RestR, me.RestL, A] = ???
 }
--- arm/src/main/scala/paperdoll/arm/SafeForRegion.scala
+++ arm/src/main/scala/paperdoll/arm/SafeForRegion.scala
@@ -8,9 +8,10 @@ import paperdoll.writer.Writer_
  * Marker that effects from the given layer can be used safely with regions
  * (managed resources).
  */
-trait SafeForRegion[L <: Layer]
+trait SafeForRegion[L /*<: Layer*/]
 
 object SafeForRegion {
-  implicit def readerSafeForRegion[I] = new SafeForRegion[Reader_[I]]{}
-  implicit def writerSafeForRegion[O] = new SafeForRegion[Writer_[O]]{}
+  implicit def readerSafeForRegion[I] = new SafeForRegion[Reader_[I]] {}
+  implicit def writerSafeForRegion[O] = new SafeForRegion[Writer_[O]] {}
+  implicit def regionSafeForRegion[S, R] = new SafeForRegion[Region_[S, R]] {}
 }
