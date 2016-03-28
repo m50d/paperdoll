93a21fbcc64d01a7e0072eb30c6094c0bb07db2c
Michael Donaghy <md401@srcf.ucam.org>
2016-03-28 08:26:44 +0100

    Prepare for next step - realise I want a test to prove it works

--- arm/src/main/scala/paperdoll/arm/Region.scala
+++ arm/src/main/scala/paperdoll/arm/Region.scala
@@ -10,7 +10,6 @@ import paperdoll.core.layer.Member
 import paperdoll.core.layer.Layers
 import shapeless.Coproduct
 import scalaz.Leibniz.===
-//import shapeless.UnaryTCConstraint
 import paperdoll.core.effect.Pure
 import scalaz.Forall
 import paperdoll.core.effect.Arr_
@@ -20,18 +19,29 @@ import paperdoll.core.queue.Queue
 import paperdoll.core.effect.Bind
 import paperdoll.core.effect.Arr
 import paperdoll.core.effect.Handler
+import scala.util.control.Exception
 import Predef.identity
 
 sealed trait Region[S, R, A] {
-  def fold[B](resource: (A === R, ManagedResource[R]) => B): B
+  def fold[B](resource: (A === R, R) => B): B
 }
+import scala.util.control.ControlThrowable
 
 object Region {
-  def newSHandle[R: Resource: Manifest](r: => R): Eff.One[Region_[S, R], R]#O forSome { type S } =
+  def newSHandle[R](r: => R): Eff.One[Region_[S, R], R]#O forSome { type S } =
     Eff.send[Region_[Any, R], R](new Region[Any, R, R] {
-      override def fold[B](resource: (R === R, ManagedResource[R]) => B) =
-        resource(Leibniz.refl, managed(r))
+      override def fold[B](resource: (R === R, R) => B) =
+        resource(Leibniz.refl, r)
     })
+    
+  /**
+   * Copied from AbstractManagedResource
+   */
+  private[this] def isRethrown(t: Throwable): Boolean = t match {
+    case _: ControlThrowable      => true
+    case _: InterruptedException  => true
+    case _                        => false    
+  }
 
   private[this] def handleInRgn[S, RE] = Eff.handle(new Bind[Region_[S, RE]] {
     override type O[X] = Either[Seq[Throwable], X]
@@ -40,7 +50,7 @@ object Region {
       Pure(Left(Seq(new RuntimeException("Opened the same handle twice. The type system is supposed to forbid this"))))
   })
 
-  def newRgn[S, RE]: Handler[Region_[S, RE]] = new Handler[Region_[S, RE]] {
+  def newRgn[S, RE: Manifest](implicit re: Resource[RE]): Handler[Region_[S, RE]] = new Handler[Region_[S, RE]] {
     override type O[X] = Either[Seq[Throwable], X]
     override def run[R <: Coproduct, L1 <: Layers[R], A](eff: Eff[R, L1, A])(
       implicit me: Member[R, Region_[S, RE]] { type L = L1 }): Eff[me.RestR, me.RestL, O[A]] =
@@ -53,11 +63,22 @@ object Region {
               otherEffect ⇒ Impure[me.RestR, me.RestL, X, O[A]](otherEffect, Queue.one[Arr_[me.RestR, me.RestL]#O, X, O[A]](
                 composed andThen { run(_) })),
               _.fold({ (le, res) =>
-                res.acquireFor {
+                managed(res).acquireFor {
                   re =>
                     //FIXME I think this is still lazy. We may need to use a lower-level interface
                     //than ManagedResource: open the resource explicitly, and then close it in the final
                     //pure case
+//                    val handle = open
+//    val result = catchingNonFatal either (f(handle))
+//    val close  = catchingNonFatal either unsafeClose(handle, result.left.toOption)
+//    // Here we pattern match to make sure we get all the errors.
+//    (result, close) match {
+//      case (Left(t1), _       ) if isRethrown(t1) => throw t1
+//      case (Left(t1), Left(t2))                   => Left(t1 :: t2 :: Nil)
+//      case (Left(t1), _       )                   => Left(t1 :: Nil)
+//      case (_,        Left(t2))                   => Left(t2 :: Nil)
+//      case (Right(r), _       )                   => Right(r)
+//    }
                     handleInRgn(le.subst[({ type K[Y] = Arr[R, L1, Y, A] })#K](composed)(re))
                 }.fold(
                   fails => Pure(Left(fails)),
