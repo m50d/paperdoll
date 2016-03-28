8866414394fe758caf4afcc865395ec29784457f
Michael Donaghy <md401@srcf.ucam.org>
2016-03-28 16:45:55 +0100

    Implement actual test for region functionality (currently failing)

--- arm/src/test/scala/paperdoll/arm/RegionTest.scala
+++ arm/src/test/scala/paperdoll/arm/RegionTest.scala
@@ -4,33 +4,35 @@ import org.junit.Test
 import scalaz.syntax.monad._
 import Region._
 import java.io.InputStreamReader
-import shapeless.{:+:, CNil}
+import shapeless.{ :+:, CNil }
 import java.io.InputStream
 import paperdoll.writer.Writer_
-import paperdoll.writer.Writer.tell
+import paperdoll.writer.Writer._
 import shapeless.Nat._0
 import shapeless.Nat._1
 import java.io.BufferedReader
 import paperdoll.core.effect.Eff
+import org.fest.assertions.Assertions.assertThat
 
 class RegionTest {
-  
+
   /**
    * This could possibly be implemented using whileM or
    * untilM or similar but I can't quite see how.
    */
   private[this] def readToWriter(br: BufferedReader): Eff.One[Writer_[String], Unit] =
     Option(br.readLine()).fold({}.point[Eff.One_[Writer_[String]]#O])({
-    line => tell(line) *> readToWriter(br)
-})
-  
-	@Test def regionWithAccessInside(): Unit = {
-	  type EffectStack = Region_[_0, InputStream] :+: Region_[_1, BufferedReader] :+: Writer_[String] :+: CNil
-	  
-	  val eff = for {
-	    inputStream <- newSHandle(_0, getClass.getResourceAsStream("names.txt")).extend[EffectStack]()
-	    reader <- newSHandle(_1, new BufferedReader(new InputStreamReader(inputStream, "UTF-8"))).extend[EffectStack]()
-	    _ <- readToWriter(reader).extend[EffectStack]()
-	  } yield {}
-	}
+      line => tell(line) *> readToWriter(br)
+    })
+
+  @Test def regionWithAccessInside(): Unit = {
+    type EffectStack = Region_[_0, InputStream] :+: Region_[_1, BufferedReader] :+: Writer_[String] :+: CNil
+    val eff = for {
+      inputStream <- newSHandle(_0, getClass.getResourceAsStream("names.txt")).extend[EffectStack]()
+      reader <- newSHandle(_1, new BufferedReader(new InputStreamReader(inputStream, "UTF-8"))).extend[EffectStack]()
+      _ <- readToWriter(reader).extend[EffectStack]()
+    } yield {}
+    val (res, written) = runWriterVector[String](newRgn[_1, BufferedReader].apply(newRgn[_0, InputStream].apply(eff))).run
+    val discard = assertThat(written).isEqualTo(Vector("Alice", "Bob", "Carol", "Eve"))
+  }
 }
