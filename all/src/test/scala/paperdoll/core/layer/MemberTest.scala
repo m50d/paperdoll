413626f6e0ffa6d4a37c5db39a1b27d5e8d434bd
Michael Donaghy <md401@srcf.ucam.org>
2016-04-01 17:07:06 +0100

    Add a basic test, which seems fine

--- all/src/test/scala/paperdoll/core/layer/MemberTest.scala
+++ all/src/test/scala/paperdoll/core/layer/MemberTest.scala
@@ -1,14 +1,20 @@
 package paperdoll.core.layer
 
 import org.junit.Test
-import shapeless.{CNil, :+:}
+import shapeless.{ CNil, :+: }
 import paperdoll.reader.Reader_
+import paperdoll.scalaz.Disjunction_
 
 class MemberTest {
-
   @Test def basicFunctionality(): Unit = {
     val _1 = Member[Reader_[Int] :+: CNil, Reader_[Int]]
     val _2 = Member[Reader_[String] :+: Reader_[Int] :+: CNil, Reader_[String]]
     val _3 = Member[Reader_[String] :+: Reader_[Int] :+: CNil, Reader_[Int]]
+  }
+
+  @Test def twoParameterType(): Unit = {
+    val _1 = Member[Disjunction_[String] :+: CNil, Disjunction_[String]]
+    val _2 = Member[Reader_[Int] :+: Disjunction_[String] :+: CNil, Disjunction_[String]]
+    val _3 = Member[Disjunction_[String] :+: Reader_[Int] :+: CNil, Disjunction_[String]]
   }
 }
