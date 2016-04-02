a6aedd457dbabd97e30d8e36e02931852b4ace54
Michael Donaghy <md401@srcf.ucam.org>
2016-04-02 17:36:23 +0100

    That'll do

--- scalaz/src/main/scala/paperdoll/scalaz/EitherTLayer.scala
+++ scalaz/src/main/scala/paperdoll/scalaz/EitherTLayer.scala
@@ -8,18 +8,17 @@ import paperdoll.core.layer.Layers
 import scalaz.Disjunction
 import scalaz.syntax.monad._
 import paperdoll.core.layer.Subset
-import paperdoll.core.layer.Member
 import Predef.implicitly
 import scalaz.Leibniz
 
 object EitherTLayer {
+  /**
+   * Inlining this method causes compilation to fail, I don't understand why
+   */
+  private[this] def sendEx[F[_], A, B](eab: Disjunction[A, B]) =
+    Eff.send[Disjunction_[A], B](eab).extend[Layer.Aux[F] :+: Disjunction_[A] :+: CNil]()
+
   def send[F[_], A, B](et: EitherT[F, A, B]): Eff[Layer.Aux[F] :+: Disjunction_[A] :+: CNil, Layers[Layer.Aux[F] :+: Disjunction_[A] :+: CNil] {
     type O[X] = F[X] :+: Disjunction[A, X] :+: CNil
-  }, B] = Eff.send[Layer.Aux[F], Disjunction[A, B]](et.run).extend[Layer.Aux[F] :+: Disjunction_[A] :+: CNil]().flatMap {
-    eab =>
-      val eff = Eff.send[Disjunction_[A], B](eab)
-      eff.extend[Layer.Aux[F] :+: Disjunction_[A] :+: CNil]()(
-          Subset[Layer.Aux[F] :+: Disjunction_[A] :+: CNil, Disjunction_[A] :+: CNil],
-          Leibniz.refl[Layers.One[Disjunction_[A]]])
-  }
+  }, B] = Eff.send[Layer.Aux[F], Disjunction[A, B]](et.run).extend[Layer.Aux[F] :+: Disjunction_[A] :+: CNil]().flatMap(sendEx[F, A, B])
 }
