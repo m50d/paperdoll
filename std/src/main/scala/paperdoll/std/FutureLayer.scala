d86c0d054f9ea753d2844436b91b90f2f2828e2a
Michael Donaghy <md401@srcf.ucam.org>
2016-04-09 08:20:25 +0100

    Simplify FutureLayer as planned
    Signed-off-by: Michael Donaghy <md401@srcf.ucam.org>

--- std/src/main/scala/paperdoll/std/FutureLayer.scala
+++ std/src/main/scala/paperdoll/std/FutureLayer.scala
@@ -6,23 +6,14 @@ import scala.concurrent.ExecutionContext
 import shapeless.{ :+:, CNil }
 import scalaz.Forall
 import paperdoll.core.effect.Arrs
+import scalaz.std.scalaFuture._
 
 object FutureLayer {
   /** We allow running stdlib Future only as the last Layer,
    *  since it's side-effecting and so can't be safely combined with laziness.
    *  For a more pure/composable notion of async,
    *  consider scalaz-concurrent Future instead
-   *  TODO replace with a call to Effects.handleLast if we have a Monad[Future]
-   *  (possibly from scalaz-outlaws?)
    */
   def runFuture[A](effects: Effects.One[Future_, A])(implicit ec: ExecutionContext): Future[A] =
-    effects.fold(Future.successful,
-      new Forall[({ type K[X] = (Future[X] :+: CNil, Arrs.One[Future_, X, A]) ⇒ Future[A] })#K] {
-        override def apply[X] = {
-          (eff, cont) ⇒
-            eff.eliminate(_.flatMap {
-              x ⇒ runFuture(Effects.compose(cont)(x))
-            }, _.impossible)
-        }
-      })
+    Effects.handleLast(effects)
 }
