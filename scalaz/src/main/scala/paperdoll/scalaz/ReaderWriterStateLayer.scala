cc3db093b38eeeae9009e5b6564df41f03cdc063
Michael Donaghy <md401@srcf.ucam.org>
2016-04-10 12:17:12 +0100

    Correct signature for ReaderWriterStateLayer
    Signed-off-by: Michael Donaghy <md401@srcf.ucam.org>

--- scalaz/src/main/scala/paperdoll/scalaz/ReaderWriterStateLayer.scala
+++ scalaz/src/main/scala/paperdoll/scalaz/ReaderWriterStateLayer.scala
@@ -1,5 +1,13 @@
 package paperdoll.scalaz
 
+import paperdoll.core.effect.Effects
+import paperdoll.core.layer.Layers
+import shapeless.{:+:, CNil}
+
 object ReaderWriterStateLayer {
-//  def sendReaderWriterState[I, W, S, A]
+  def sendReaderWriterState[I, W, S, A]: Effects[
+    Reader_[I] :+: Writer_[W] :+: State_[S] :+: CNil,
+    Layers[Reader_[I] :+: Writer_[W] :+: State_[S] :+: CNil] {
+      type O[X] = Reader_[I]#F[X] :+: Writer_[W]#F[X] :+: State_[S]#F[X] :+: CNil
+    }, A] = ???
 }
