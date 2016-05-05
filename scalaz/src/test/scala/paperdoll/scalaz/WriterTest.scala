210472ca9af90956d14a925df72ed66544435723
Michael Donaghy <md401@srcf.ucam.org>
2016-05-05 20:19:34 +0100

    This much is inferred at least
    Signed-off-by: Michael Donaghy <md401@srcf.ucam.org>

--- scalaz/src/test/scala/paperdoll/scalaz/WriterTest.scala
+++ scalaz/src/test/scala/paperdoll/scalaz/WriterTest.scala
@@ -38,7 +38,7 @@ class WriterTest {
     ](extendedEffect)(Member.nil[Writer_[Int], Layer.Aux[Tgt] :+: CNil], Leibniz.refl[Layers.Two[Writer_[Int], Layer.Aux[Tgt]]],
         Subset.consSubset[Layer.Aux[Tgt] :+: CNil, Layer.Aux[Tgt], Layers.One[Layer.Aux[Tgt]], CNil, Layers.One[Layer.Aux[Tgt]]](
           Member.nil[Layer.Aux[Tgt], CNil],
-            Subset.nilSubset[Layer.Aux[Tgt] :+: CNil], Leibniz.refl[Layers.One[Layer.Aux[Tgt]]]
+            Subset.nilSubset[Layer.Aux[Tgt] :+: CNil], implicitly
         ))
     val overall = unsafeRun(translatedEffect)
     val _1 = assertThat(overall.run).isEqualTo(Some((6, "WriterTest")))
