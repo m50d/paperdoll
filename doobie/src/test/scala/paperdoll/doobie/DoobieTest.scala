db5cc1e2056087646fbd01491e971b95302364d7
Michael Donaghy <md401@srcf.ucam.org>
2016-05-21 22:22:57 +0100

    Define catchable/capture instances
    Signed-off-by: Michael Donaghy <md401@srcf.ucam.org>

--- doobie/src/test/scala/paperdoll/doobie/DoobieTest.scala
+++ doobie/src/test/scala/paperdoll/doobie/DoobieTest.scala
@@ -5,9 +5,18 @@ import paperdoll.scalaz.concurrent.Task_
 import paperdoll.core.effect.Effects
 import scalaz.Catchable
 import scalaz.concurrent.Task
+import paperdoll.core.effect.Effects.{sendU, unsafeRun}
 
 class DoobieTest {
-  val xa = DriverManagerTransactor[Task](
+  implicit object catchable extends Catchable[Effects.One_[Task_]#O] {
+    override def attempt[A](f: Effects.One[Task_, A]) = sendU(Task.taskInstance.attempt(unsafeRun(f)))
+    override def fail[A](err: Throwable) = sendU(Task.taskInstance.fail[A](err))
+  }
+  implicit object capture extends Capture[Effects.One_[Task_]#O] {
+    override def apply[A](a: => A) = sendU(Task.delay(a))
+  }
+  
+  val xa = DriverManagerTransactor[Effects.One_[Task_]#O](
   "org.h2.Driver", "jdbc:h2:mem", "", "sa"
 )
 }
