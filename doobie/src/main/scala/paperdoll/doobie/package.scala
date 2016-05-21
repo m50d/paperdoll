f9da55c42e6f04d0f94eb6ee97363b70cfb57769
Michael Donaghy <md401@srcf.ucam.org>
2016-05-21 09:15:31 +0100

    Add aliases for other parts of doobie
    Signed-off-by: Michael Donaghy <md401@srcf.ucam.org>

--- doobie/src/main/scala/paperdoll/doobie/package.scala
+++ doobie/src/main/scala/paperdoll/doobie/package.scala
@@ -1,10 +1,19 @@
 package paperdoll
 
 import paperdoll.core.layer.Layer
-import _root_.doobie.imports.ConnectionIO
+import _root_.doobie.imports._
 
 package object doobie {
   type ConnectionIO_ = Layer {
     type F[X] = ConnectionIO[X]
+  }
+  type PreparedStatementIO_ = Layer {
+    type F[X] = PreparedStatementIO[X]
+  }
+  type StatementIO_ = Layer {
+    type F[X] = StatementIO[X]
+  }
+  type ResultSetIO_ = Layer {
+    type F[X] = ResultSetIO[X]
   }
 }
